56ee191291e430c255f6952696fd506d430359b4
ErickGiffoni <giffoni.erick@gmail.com>
2020-12-05 18:10:37 -0300

    adiciona estrutura inicial

---
+++ front/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-app>
+        <v-main class="content">
+            <TruckMe/>
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+import TruckMe from './views/Truck-Me';
+
+export default {
+  name: 'App',
+
+  components: {
+    MSCRenderer,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
+<style lang="scss">
+.content {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
---
+++ front/src/views/TruckMe.vue
@@ -0,0 +1,57 @@
+<template>
+    <v-container class="d-flex flex-column align-center justify-center mb-4">
+        <v-text-field
+            
+            class="text-center"
+            v-model="peso"
+            label="Qual a importância desse caminhão ?<br>
+            1 - leve; 2 - leviana; 3 - média; 4 - muita; 5 - urgente" />
+
+        <v-text-field
+            class="text"
+            v-model="horaChegada"
+            label="Qual o horário de chegada do caminhão ?" />
+        
+        <v-text-field
+            class="text"
+            v-model="horaSaida"
+            label="Qual o horário de saida do caminhão ?" />
+        
+    </v-container>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        peso: "",
+        horaChegada: "",
+        horaSaida: ""
+    }),
+    computed: {
+        maiorSubSequencia() {
+            return this.calculaMaiorSubsequencia(this.sequencia).max
+        },
+    },
+    methods: {
+        ordenaPorTempoTermino(){
+            return 0
+        },
+        computaPesos(){
+            return 0
+        },
+        encontraSolucao(){
+            return 0
+        },
+    },
+}
+</script>
+
+<style lang="scss">
+.v-input {
+    width: 100%;
+
+    input {
+        text-align: center;
+    }
+}
+</style>
